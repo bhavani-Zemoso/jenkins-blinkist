af417f6da4ce63cf772330d31d057f642425352c
bhavani-Zemoso <bhavani.somanchi@zemosolabs.com>
2023-02-13 14:57:26 +0530

    Added cypress file

---
+++ frontend/cypress/e2e/amazon.cy.ts
@@ -0,0 +1,7 @@
+context('Amazon Application testing with Cypress', () => {
+	describe('Amazon Application testing After login', () => {
+		it('Enable prime option for items', () => {
+			cy.visit('www.amazon.in');
+		});
+	});
+});
